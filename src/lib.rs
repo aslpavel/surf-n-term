ed5e99c0e50d2b094bb14bcc2aa7de7e95e43a75
Pavel Aslanov <asl.pavel@gmail.com>
2021-05-08 00:04:21 +0100

    [doc] top level module docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! This crate is used to interract with Posix terminal. It can be used to
+//!  - Read events from the terminal
+//!  - Send commands to the terminal
+//!  - Render on a surface which will be reconciled with current content of the terminal
+//!  - Issue direct commends to the terminal
+//!  - Supports kitty/sixel image protocol
+//!
+//! ### Simple example
+//! ```rust
+//! use surf_n_term::{Terminal, TerminalEvent, Error};
+//!
+//! fn main() -> Result<(), Error> {
+//!     let ctrl_c = TerminalEvent::Key("ctrl+c".parse()?);
+//!     let mut term = SystemTerminal::new()?;
+//!     term.run_render(|term, event, mut view| -> Result<_, Error> {
+//!         // This function will be executed on each event from terminal
+//!         // - term  - implementes Terminal trait
+//!         // - event - is a TerminalEvent
+//!         // - view  - is a Suface that can be used to render on, see render module for defails
+//!         match event {
+//!             Some(event) if &event == &ctrl_c => {
+//!                 // exit if 'ctrl+c' is pressed
+//!                 Ok(TerminalAction::Quit(()))
+//!             }
+//!             _ => {
+//!                 // do some rendering by updating the view
+//!                 Ok(TerminalAction::Wait)
+//!             },
+//!         }
+//!     })?;
+//!     Ok(())
+//! }
+//! ```
 #![allow(clippy::type_complexity)]
 #![allow(clippy::reversed_empty_ranges)]
 #![deny(warnings)]
